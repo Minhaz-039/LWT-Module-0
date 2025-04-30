c0f24f805f48ca415283103b1e3a95bf5c96485a
Minhaz-039 <minhazreza3243@gmail.com>
2025-05-01 05:10:16 +0600

    Mainly created this project to learn how to add tailwind css with vite project

---
+++ Tailwind_V4/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold bg-mint-500">
+        This is a practice tailwind Css
+      </h1>
+    </>
+  );
+}
+
+export default App;
